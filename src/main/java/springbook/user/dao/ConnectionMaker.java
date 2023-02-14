4c789ddae41c7f15da8009345626bdf24643df7b
Sangkook Lee <ssalguk@gmail.com>
2023-02-14 21:09:39 +0900

    fix: Cleanup dao replace with xml config

--- src/main/java/springbook/user/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package springbook.user.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
